011eaf9566f9df8592efe2a65c41882286f9bad5
jechamreytc <jechamreycabusog69@gmail.com>
2025-08-05 18:56:07 +0800

    added CreateInvoice and Display Invoice

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import BookingChargesList from './frontdesk/BookingChargesList';
 import BookingRequestList from './frontdesk/BookingListRequest';
 import BookingChargesMaster from './frontdesk/BookingChargesMaster';
+import BookingCreateInvoice from './frontdesk/BookingCreateInvoice';
+import BookingDisplayInvoiceSample from './frontdesk/BookingDisplayInvoiceSample';
 
 
 
@@ -21,6 +23,8 @@ function App() {
           <Route path="/BookingChargesMaster" element={<BookingChargesMaster/>} />
           <Route path="/" element={<BookingRequestList/>} />
           <Route path="/" element={<BookingChargesList />} />
+          <Route path="/BookingCreateInvoice" element={<BookingCreateInvoice />} />
+          <Route path="/BookingDisplayInvoiceSample" element={<BookingDisplayInvoiceSample />} />
         </Routes>
       </BrowserRouter>
     </div>
