7c81119f115ed75f4619b9b3abe76b8bb614fb8d
razzibazzi <baldozarazieljade96@gmail.com>
2025-08-05 16:53:22 +0800

    demiren-frontdesk

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import BookingChargesList from './frontdesk/BookingChargesList';
+import BookingRequestList from './frontdesk/BookingListRequest';
+import BookingChargesMaster from './frontdesk/BookingChargesMaster';
+
+
 
 function App() {
+  useEffect(() => {
+    if (localStorage.getItem('url') !== 'http://localhost/demiren/api/') {
+      localStorage.setItem('url', 'http://localhost/demiren/api/');
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/BookingChargesMaster" element={<BookingChargesMaster/>} />
+          <Route path="/" element={<BookingRequestList/>} />
+          <Route path="/" element={<BookingChargesList />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
